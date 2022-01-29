3526546368dfa3f414dd12fd5098aa8742b778a5
zhekaasd <zhekaasd13@gmail.com>
2022-01-29 08:35:58 +0400

    Updates

---
+++ precache-manifest.b4317e842023815660bdcf0fb888ba98.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ea473d6a7f1f5933aa9708c3b6b135",
+    "url": "/hwi/index.html"
+  },
+  {
+    "revision": "33fec55cf7fb49641c8c",
+    "url": "/hwi/static/css/main.cfa72f51.chunk.css"
+  },
+  {
+    "revision": "a45fee3d2d90d6b70a97",
+    "url": "/hwi/static/js/2.e67589fa.chunk.js"
+  },
+  {
+    "revision": "83dd22b0688da01307759f0db1576406",
+    "url": "/hwi/static/js/2.e67589fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fec55cf7fb49641c8c",
+    "url": "/hwi/static/js/main.da915251.chunk.js"
+  },
+  {
+    "revision": "55a0d3f177cab153bc6b",
+    "url": "/hwi/static/js/runtime-main.1c31adfe.js"
+  },
+  {
+    "revision": "24f1671b0f3f6c2418db36a9270b1506",
+    "url": "/hwi/static/media/3fcf565ccc553afcfd89858c97304705.24f1671b.gif"
+  },
+  {
+    "revision": "081c4bec113d3dc81a872b902fd806b5",
+    "url": "/hwi/static/media/frown-open-solid.081c4bec.svg"
+  },
+  {
+    "revision": "bd3b118569faa3632959173b661f8745",
+    "url": "/hwi/static/media/frown-solid.bd3b1185.svg"
+  },
+  {
+    "revision": "722af8ac2254fea2de0d22c78f481898",
+    "url": "/hwi/static/media/sad-tear-solid.722af8ac.svg"
+  },
+  {
+    "revision": "28851b1e854436849ebead284a8dc984",
+    "url": "/hwi/static/media/trash-solid.28851b1e.svg"
+  }
+]);
